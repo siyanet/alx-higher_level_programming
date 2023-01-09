3b514600046311ac73e009a4865082856d73ff64
siyanet <siyangetahunw@gmail.com>
2023-01-09 12:56:48 -0300

    8-multiple_returns.py file is added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,3 @@ def multiple_returns(sentence):
     if i == 0:
         return i, None
     return i,sentence[0]
-
-    sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
