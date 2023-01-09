66981b5f9dffb3d291d4689b2582e68dba4a1d50
siyanet <siyangetahunw@gmail.com>
2023-01-09 15:19:02 -0300

    6-print_matrix_integer.py file is debugged

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print(col, end=" ")
-        print("")
+            if col == row[-1]:
+                print("{:d}".format(col), end="")
+            print("{:d}".format(col), end=" ")
+        print()
